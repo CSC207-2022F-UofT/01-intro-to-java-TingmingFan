e5dcae43e3a803ba9499d0c11c751722034f2177
TingmingFan <tingming.fan@mail.utoronto.ca>
2022-10-08 14:06:08 -0400

    ting

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -8,7 +8,7 @@
  */
 
 /**
- *  1. In Java, no code can exist outside of a class. Unlike Python which uses
+ *  1. In Java, no code can exist outside a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
  *
  *    For this exercise, all of our code is within the Basics class.
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,8 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
+
 
 
 
@@ -99,9 +102,11 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
-
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: "+i);
+        }
     }
+
 
     /**
      *  6. Below is a method named split which returns a String,
@@ -141,11 +146,11 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        for (String s : to_split.split(" ")) {
+            ret.append(s.charAt(0));
+        }return ret.toString();
+        }
 
-        // Fill in the rest of the body here
-
-        return ret.toString();
-    }
 
     /**
      * 7. Below is a method that takes in an integer array (similar to a list
@@ -170,7 +175,13 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        for (int i = 0; i < arr.length; i ++) {
+            if (i % 2 != 0) {
+                current_sum += arr[i];
+            } else if (arr.length <= 1) {
+                return 0;
+            }
+        }
         return current_sum;
     }
 
